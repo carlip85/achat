8f028ad9045516bfb93ae629d7702cf464ad6851
Trillius <23412130+Trillius@users.noreply.github.com>
2021-12-11 02:46:32 +0000

    Add files via upload

---
+++ makeJSON2.js
@@ -0,0 +1,83 @@
+function clearContents(element) {
+	element.value = '';
+}
+  
+function raiseHand(idX){
+  var jobValue = document.getElementById('msgTxt').value
+	var jsonPost=(JSON.stringify({text:jobValue,button:idX}))
+	sendJSON(jobValue,idX);
+}
+  
+function sendJSON(msgTxt,btnID){
+switch(btnID){
+  case "koyySig":
+    document.getElementById("koyySig").style.boxShadow="0px 0px 15px 3px rgba(255,255,255,0.9);"
+  break;
+  case "kfgoSig":
+  break;
+  case "kvoxSig":
+  break;
+}
+
+console.log(msgTxt,btnID);
+fetch('/', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+    body: JSON.stringify({
+      read:1,button:btnID
+    }),
+  })
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.log(error);
+});
+}
+
+function displayRadioValue() {
+  return($("input[type='radio'][name='test']:checked").val());
+}
+function enableChat() {
+  document.getElementById("chatEnter").disabled = false;;
+  document.getElementById("msg").disabled = false;;
+}
+
+function makeChange2(inp) {
+  var buttonElement;
+  var nodes = document.getElementById('sigBox').childNodes;
+  
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].id != inp) { 
+      buttonElement = nodes[i].firstElementChild;
+      if (buttonElement) {   
+        buttonElement.classList.remove("btn-enable");
+        buttonElement.classList.add("btn-disable");
+      }
+    }
+  }
+  $("button[id=" + inp + "]").toggleClass("btn-enable");
+}
+
+function makeChange(inp){
+  //enable button
+    $("button[id="+inp+"]").addClass("btn-enable");
+    $("button[id="+inp+"]").removeClass("btn-disable");
+    $("button[id="+inp+"]").prop('disabled',false);
+  //disable button
+    $("button[id!="+inp+"]").removeClass("btn-enable");
+    $("button[id!="+inp+"]").addClass("btn-disable");
+    $("button[id!="+inp+"]").prop('disabled',true);
+  //ignore chat button
+    $("button[id=chatEnter]").removeClass("btn-disable");
+}
+
+function enterChat(inp){
+  console.log({"station":displayRadioValue(),"userName":fname.value,"msg":msg.value});
+  var textcontrol = document.getElementById("msg");
+            textcontrol.value = "";
+}
+  
